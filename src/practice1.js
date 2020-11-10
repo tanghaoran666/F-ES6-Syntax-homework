30a79e2bd48d1ed8cfc7108ffddb3d2baf059641
tanghaoran <407458281@qq.com>
2020-11-10 23:24:59 +0800

    implement

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+  const persons = [];
+  for (let v of input.data) {
+    const person = {};
+    let index = 0;
+    for (let c of input.column) {
+      person[c.name] = v[index];
+      index++;
+    }
+    persons.push(person);
+  }
+  return persons;
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+  let flag = 0;
+  for (let section of sections) {
+    const { content, index } = section;
+    items.splice(index + flag, 1, ...[content, items[index + flag]]);
+    flag++;
+  }
+  return items;
+};
 export { inject };
